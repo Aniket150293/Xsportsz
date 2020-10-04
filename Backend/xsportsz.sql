1e03a708e15f24ad4a740c51bba804e101bfb079
Aniket150293 <aniketbansode15@gmail.com>
2020-10-04 18:47:13 +0530

    New Db Script

--- Backend/xsportsz.sql
+++ Backend/xsportsz.sql
@@ -1,3 +1,21 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 04, 2020 at 03:15 PM
+-- Server version: 5.6.17
+-- PHP Version: 5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
 --
 -- Database: `xsportsz`
 --
@@ -5,48 +23,75 @@
 -- --------------------------------------------------------
 
 --
--- Table structure for table `state_master`
+-- Table structure for table `specialization_master`
 --
 
-CREATE TABLE `state_master` (
-  `id` int(10) PRIMARY KEY AUTO_INCREMENT NOT NULL,
-  `state` varchar(150) NOT NULL,
-  `is_active` tinyint(1) NOT NULL
-);
+CREATE TABLE IF NOT EXISTS `specialization_master` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `sport_id` int(10) NOT NULL,
+  `name` varchar(500) NOT NULL,
+  `is_active` tinyint(4) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
+
+--
+-- Dumping data for table `specialization_master`
+--
+
+INSERT INTO `specialization_master` (`id`, `sport_id`, `name`, `is_active`) VALUES
+(1, 1, 'Batsman', 1),
+(2, 1, 'Bowler', 1),
+(3, 1, 'Wicket Keeper', 1),
+(4, 2, 'Goal Keeper - Football', 1),
+(5, 2, 'Defender - Football', 1),
+(6, 2, 'Striker - Football', 1),
+(7, 2, 'Mid Fielder - Football', 1),
+(8, 4, 'Goal Keeper - Hockey', 1);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `transaction_details`
+-- Table structure for table `sports_master`
 --
 
-CREATE TABLE `transaction_details` (
-  `id` int(10) PRIMARY KEY AUTO_INCREMENT NOT NULL,
-  `transaction_id` varchar(100) DEFAULT NULL,
-  `mode_of_transfer` tinyint(1) DEFAULT NULL,
-  `type_of_transfer` tinyint(1) DEFAULT NULL,
-  `from_account` int(11) NOT NULL,
-  `to_account` int(11) NOT NULL,
-  `amount` bigint(20) NOT NULL,
-  `from_bank_id` int(10) DEFAULT NULL,
-  `to_bank_id` int(10) DEFAULT NULL,
-  `from_user_id` int(10) DEFAULT NULL,
-  `to_user_id` int(10) DEFAULT NULL,
-  `performed_date` datetime DEFAULT NULL,
-  `is_pending` tinyint(1) DEFAULT NULL,
-  `is_cancelled` tinyint(1) DEFAULT NULL,
-  `is_approved` tinyint(1) DEFAULT NULL,
-  `performed_by` int(11) DEFAULT NULL
-);
+CREATE TABLE IF NOT EXISTS `sports_master` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(500) NOT NULL,
+  `is_active` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
--- ---------------------------------------------------------- --------------------------------------------------------
+--
+-- Dumping data for table `sports_master`
+--
+
+INSERT INTO `sports_master` (`id`, `name`, `is_active`) VALUES
+(1, 'Cricket', 1),
+(2, 'Football', 1),
+(3, 'Tennis', 1),
+(4, 'Hockey', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `state_master`
+--
+
+CREATE TABLE IF NOT EXISTS `state_master` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `state` varchar(150) NOT NULL,
+  `is_active` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `user_details`
 --
 
-CREATE TABLE `user_details` (
-  `id` int(10) PRIMARY KEY AUTO_INCREMENT NOT NULL,
+CREATE TABLE IF NOT EXISTS `user_details` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
   `profile_created_by` varchar(15) NOT NULL,
   `profile` varchar(100) NOT NULL,
   `email` varchar(50) NOT NULL,
@@ -66,25 +111,33 @@ CREATE TABLE `user_details` (
   `pan_number` varchar(100) NOT NULL,
   `adhar_number` varchar(1000) NOT NULL,
   `role` varchar(10) NOT NULL,
-  `is_active` tinyint(1) NOT NULL DEFAULT 1,
-  `is_blocked` tinyint(1) NOT NULL DEFAULT 0,
+  `is_active` tinyint(1) NOT NULL DEFAULT '1',
+  `is_blocked` tinyint(1) NOT NULL DEFAULT '0',
   `last_login` datetime NOT NULL,
   `created_at` datetime NOT NULL,
-  `modified_at` datetime NOT NULL
-);
+  `modified_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user_profiles`
+-- Table structure for table `user_sport_mapping`
 --
 
-CREATE TABLE `user_profiles` (
-  `id` varchar(100) PRIMARY KEY AUTO_INCREMENT NOT NULL,
-  `size` varchar(11) NOT NULL,
-  `image` varchar(100) NOT NULL,
-  `url` varchar(500) NOT NULL,
-  `is_active` tinyint(1) NOT NULL DEFAULT 1
-);
+CREATE TABLE IF NOT EXISTS `user_sport_mapping` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) NOT NULL,
+  `sport_id` int(10) NOT NULL,
+  `years_age` int(10) NOT NULL,
+  `months_age` int(10) NOT NULL,
+  `specialization_id` int(10) NOT NULL,
+  `is_active` tinyint(1) NOT NULL DEFAULT '1',
+  `created_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
--- --------------------------------------------------------
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
